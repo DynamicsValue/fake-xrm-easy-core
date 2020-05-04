711a48182316c67a48122fae68c6933920ca267b
jordimontana82 <jordi.montana@gmail.com>
2020-05-04 04:24:49 +0200

    Updated QueryByAttributeTests to use new middleware

--- tests/FakeXrmEasy.Core.Tests/FakeContextTests/QueryByAttribute/QueryByAttributeTests.cs
+++ tests/FakeXrmEasy.Core.Tests/FakeContextTests/QueryByAttribute/QueryByAttributeTests.cs
@@ -1,4 +1,6 @@
 ﻿using Crm;
+using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Middleware;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Query;
@@ -12,12 +14,17 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
 {
     public class Tests
     {
-        [Fact]
-        public static void Order_is_respected_for_query_by_attribute()
+        private readonly IXrmFakedContext _context;
+        private readonly IOrganizationService _service;
+        public Tests()
         {
-            var fakedContext = new XrmFakedContext();
-            var fakedService = fakedContext.GetOrganizationService();
+            _context = XrmFakedContextFactory.New();
+            _service = _context.GetOrganizationService();
+        }
 
+        [Fact]
+        public void Order_is_respected_for_query_by_attribute()
+        {
             var contact1 = new Contact
             {
                 Id = Guid.NewGuid(),
@@ -32,14 +39,14 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 BirthDate = new DateTime(1981)
             };
 
-            fakedContext.Initialize(new List<Entity> { contact1, contact2 });
+            _context.Initialize(new [] { contact1, contact2 });
 
             QueryByAttribute query = new QueryByAttribute("contact")
             {
                 ColumnSet = new ColumnSet("lastname", "birthdate")
             };
             query.AddOrder("birthdate", OrderType.Descending);
-            var results = fakedService.RetrieveMultiple(query);
+            var results = _service.RetrieveMultiple(query);
 
             var ordered = results.Entities.Cast<Contact>().ToArray();
 
@@ -48,17 +55,14 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
         }
 
         [Fact]
-        public static void When_a_query_by_attribute_is_executed_with_one_attribute_right_result_is_returned()
+        public void When_a_query_by_attribute_is_executed_with_one_attribute_right_result_is_returned()
         {
-            var context = new XrmFakedContext();
             var account = new Account() { Id = Guid.NewGuid(), Name = "Test" };
             var account2 = new Account() { Id = Guid.NewGuid(), Name = "Other account!" };
-            context.Initialize(new List<Entity>()
+            _context.Initialize(new List<Entity>()
             {
                 account, account2
             });
-
-            var service = context.GetOrganizationService();
 
             QueryByAttribute query = new QueryByAttribute();
             query.Attributes.AddRange(new string[] { "name" });
@@ -69,24 +73,21 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
             //Execute using a request to test the OOB (XRM) message contracts
             RetrieveMultipleRequest request = new RetrieveMultipleRequest();
             request.Query = query;
-            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse) _service.Execute(request)).EntityCollection.Entities;
 
             Assert.True(entityList.Count == 1);
             Assert.Equal(entityList[0]["name"].ToString(), "Test");
         }
 
         [Fact]
-        public static void When_a_query_by_a_boolean_attribute_is_executed_with_one_attribute_right_result_is_returned()
+        public void When_a_query_by_a_boolean_attribute_is_executed_with_one_attribute_right_result_is_returned()
         {
-            var context = new XrmFakedContext();
             var account = new Account() { Id = Guid.NewGuid(), Name = "Test", DoNotEMail = false };
             var account2 = new Account() { Id = Guid.NewGuid(), Name = "Other account!", DoNotEMail = true };
-            context.Initialize(new List<Entity>()
+            _context.Initialize(new List<Entity>()
             {
                 account, account2
             });
-
-            var service = context.GetOrganizationService();
 
             var query = new QueryByAttribute
             {
@@ -99,18 +100,15 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
             //Execute using a request to test the OOB (XRM) message contracts
             RetrieveMultipleRequest request = new RetrieveMultipleRequest();
             request.Query = query;
-            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse) _service.Execute(request)).EntityCollection.Entities;
 
             Assert.True(entityList.Count == 1);
             Assert.Equal(entityList[0]["name"].ToString(), "Test");
         }
 
         [Fact]
-        public static void When_a_query_by_attribute_is_executed_with_one_null_attribute_it_is_not_returned()
+        public void When_a_query_by_attribute_is_executed_with_one_null_attribute_it_is_not_returned()
         {
-            var fakedContext = new XrmFakedContext();
-            var fakedService = fakedContext.GetOrganizationService();
-
             var contact = new Contact
             {
                 Id = Guid.NewGuid(),
@@ -118,44 +116,38 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 LastName = "asdf"
             };
 
-            fakedContext.Initialize(new List<Entity> { contact });
+            _context.Initialize(new List<Entity> { contact });
 
             QueryByAttribute query = new QueryByAttribute("contact");
             query.ColumnSet = new ColumnSet("firstname", "lastname");
-            var results = fakedService.RetrieveMultiple(query);
+            var results = _service.RetrieveMultiple(query);
 
             Assert.True(results.Entities[0].Attributes.ContainsKey("lastname"));
             Assert.False(results.Entities[0].Attributes.ContainsKey("firstname"));
         }
 
         [Fact]
-        public static void When_a_query_by_attribute_is_executed_with_non_existing_attribute_it_is_not_returned()
+        public void When_a_query_by_attribute_is_executed_with_non_existing_attribute_it_is_not_returned()
         {
-            var fakedContext = new XrmFakedContext();
-            var fakedService = fakedContext.GetOrganizationService();
-
             var contact = new Contact
             {
                 Id = Guid.NewGuid(),
                 LastName = "asdf"
             };
 
-            fakedContext.Initialize(new List<Entity> { contact });
+            _context.Initialize(new List<Entity> { contact });
 
             QueryByAttribute query = new QueryByAttribute("contact");
             query.ColumnSet = new ColumnSet("firstname", "lastname");
-            var results = fakedService.RetrieveMultiple(query);
+            var results = _service.RetrieveMultiple(query);
 
             Assert.True(results.Entities[0].Attributes.ContainsKey("lastname"));
             Assert.False(results.Entities[0].Attributes.ContainsKey("firstname"));
         }
 
         [Fact]
-        public static void Page_info_is_respected_for_query_by_attribute()
+        public void Page_info_is_respected_for_query_by_attribute()
         {
-            var fakedContext = new XrmFakedContext();
-            var fakedService = fakedContext.GetOrganizationService();
-
             var contact1 = new Contact
             {
                 Id = Guid.NewGuid(),
@@ -168,7 +160,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 LastName = "qwer"
             };
 
-            fakedContext.Initialize(new List<Entity> { contact1, contact2 });
+            _context.Initialize(new List<Entity> { contact1, contact2 });
 
             QueryByAttribute query = new QueryByAttribute("contact");
             query.ColumnSet = new ColumnSet("firstname", "lastname");
@@ -177,18 +169,15 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 PageNumber = 1,
                 Count = 1,
             };
-            var results = fakedService.RetrieveMultiple(query);
+            var results = _service.RetrieveMultiple(query);
 
             Assert.True(results.MoreRecords);
             Assert.NotNull(results.PagingCookie);
         }
 
         [Fact]
-        public static void Return_total_record_count_is_respected_for_query_by_attribute()
+        public void Return_total_record_count_is_respected_for_query_by_attribute()
         {
-            var fakedContext = new XrmFakedContext();
-            var fakedService = fakedContext.GetOrganizationService();
-
             var contact1 = new Contact
             {
                 Id = Guid.NewGuid(),
@@ -201,7 +190,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 LastName = "qwer"
             };
 
-            fakedContext.Initialize(new List<Entity> { contact1, contact2 });
+            _context.Initialize(new List<Entity> { contact1, contact2 });
 
             QueryByAttribute query = new QueryByAttribute("contact");
             query.ColumnSet = new ColumnSet("firstname", "lastname");
@@ -211,7 +200,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryByAttributeTests
                 Count = 1,
                 ReturnTotalRecordCount = true
             };
-            var results = fakedService.RetrieveMultiple(query);
+            var results = _service.RetrieveMultiple(query);
 
             Assert.Equal(1, results.Entities.Count);
             Assert.Equal(2, results.TotalRecordCount);
