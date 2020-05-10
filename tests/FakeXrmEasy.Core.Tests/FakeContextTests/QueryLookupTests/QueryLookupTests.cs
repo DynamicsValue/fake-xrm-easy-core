f94318c902f8044372fb331d5fb7a96fadac9d30
jordimontana82 <jordi.montana@gmail.com>
2020-05-10 20:50:28 +0200

    Updated QueryLookupTests to use new middleware

--- tests/FakeXrmEasy.Core.Tests/FakeContextTests/QueryLookupTests/QueryLookupTests.cs
+++ tests/FakeXrmEasy.Core.Tests/FakeContextTests/QueryLookupTests/QueryLookupTests.cs
@@ -1,4 +1,6 @@
 ﻿using Crm;
+using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Middleware;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Query;
@@ -11,20 +13,26 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryLookupTests
 {
     public class Tests
     {
-        [Fact]
-        public static void When_a_query_on_lookup_is_executed_with_a_guid_right_result_is_returned()
+        private readonly IXrmFakedContext _context;
+        private readonly IOrganizationService _service;
+        
+        public Tests()
         {
-            var context = new XrmFakedContext();
+            _context = XrmFakedContextFactory.New();
+            _service = _context.GetOrganizationService();
+        }
+
+        [Fact]
+        public void When_a_query_on_lookup_is_executed_with_a_guid_right_result_is_returned()
+        {
             var user = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "User" };
             var user2 = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "Other user!" };
             var account = new Account() { Id = Guid.NewGuid(), Name = "Test", ["createdby"] = user.ToEntityReference() };
             var account2 = new Account() { Id = Guid.NewGuid(), Name = "Other account!", ["createdby"] = user2.ToEntityReference() };
-            context.Initialize(new List<Entity>()
+            _context.Initialize(new List<Entity>()
             {
                 user, user2, account, account2
             });
-
-            var service = context.GetOrganizationService();
 
             QueryExpression query = new QueryExpression();
             query.ColumnSet = new ColumnSet(new string[] { "name" });
@@ -34,26 +42,23 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryLookupTests
             //Execute using a request to test the OOB (XRM) message contracts
             RetrieveMultipleRequest request = new RetrieveMultipleRequest();
             request.Query = query;
-            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse)_service.Execute(request)).EntityCollection.Entities;
 
             Assert.True(entityList.Count == 1);
             Assert.Equal(entityList[0]["name"].ToString(), "Test");
         }
 
         [Fact]
-        public static void When_a_query_on_lookup_is_executed_with_a_guid_as_string_right_result_is_returned()
+        public void When_a_query_on_lookup_is_executed_with_a_guid_as_string_right_result_is_returned()
         {
-            var context = new XrmFakedContext();
             var user = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "User" };
             var user2 = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "Other user!" };
             var account = new Account() { Id = Guid.NewGuid(), Name = "Test", ["createdby"] = user.ToEntityReference() };
             var account2 = new Account() { Id = Guid.NewGuid(), Name = "Other account!", ["createdby"] = user2.ToEntityReference() };
-            context.Initialize(new List<Entity>()
+            _context.Initialize(new List<Entity>()
             {
                 user, user2, account, account2
             });
-
-            var service = context.GetOrganizationService();
 
             QueryExpression query = new QueryExpression();
             query.ColumnSet = new ColumnSet(new string[] { "name" });
@@ -63,26 +68,24 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryLookupTests
             //Execute using a request to test the OOB (XRM) message contracts
             RetrieveMultipleRequest request = new RetrieveMultipleRequest();
             request.Query = query;
-            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse)_service.Execute(request)).EntityCollection.Entities;
 
             Assert.True(entityList.Count == 1);
             Assert.Equal(entityList[0]["name"].ToString(), "Test");
         }
 
         [Fact]
-        public static void When_a_query_on_lookup_is_executed_with_name_suffixed_right_result_is_returned()
+        public void When_a_query_on_lookup_is_executed_with_name_suffixed_right_result_is_returned()
         {
-            var context = new XrmFakedContext();
             var user = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "User" };
             var user2 = new SystemUser { Id = Guid.NewGuid(), ["fullname"] = "Other user!" };
             var account = new Account() { Id = Guid.NewGuid(), Name = "Test", ["createdby"] = new EntityReference(user.LogicalName, user.Id) { Name = user.FullName } };
             var account2 = new Account() { Id = Guid.NewGuid(), Name = "Other account!", ["createdby"] = new EntityReference(user.LogicalName, user2.Id) { Name = user2.FullName } };
-            context.Initialize(new List<Entity>()
+            
+            _context.Initialize(new List<Entity>()
             {
                 user, user2, account, account2
             });
-
-            var service = context.GetOrganizationService();
 
             QueryExpression query = new QueryExpression();
             query.ColumnSet = new ColumnSet(new string[] { "name" });
@@ -92,7 +95,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.QueryLookupTests
             //Execute using a request to test the OOB (XRM) message contracts
             RetrieveMultipleRequest request = new RetrieveMultipleRequest();
             request.Query = query;
-            Collection<Entity> entityList = ((RetrieveMultipleResponse)service.Execute(request)).EntityCollection.Entities;
+            Collection<Entity> entityList = ((RetrieveMultipleResponse)_service.Execute(request)).EntityCollection.Entities;
 
             Assert.True(entityList.Count == 1);
             Assert.Equal(entityList[0]["name"].ToString(), "Test");
