cd0d69bddcb4cbbe477d85a55b6fe4dfc623c0ac
Jordi <jordi.montana@gmail.com>
2020-12-28 17:44:10 -0800

    Added multiple configurations and reference to abstractions

--- restore.ps1
+++ restore.ps1
@@ -16,9 +16,12 @@ Write-Host "Deleting previous installed packages in '$($localPackageRestoreFolde
 Remove-Item -Recurse -Force $localPackageRestoreFolder
 
 Write-Host "Restoring packages..."
-dotnet restore -p:TargetFrameworks=$targetFrameworks
-if(!($LASTEXITCODE -eq 0)) {
-    throw "Error restoring packages"
-}
+
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY" -targetFrameworks $targetFrameworks
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -targetFrameworks $targetFrameworks
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -targetFrameworks $targetFrameworks
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -targetFrameworks $targetFrameworks
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
+./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
 
 Write-Host "Done. It's convenient to restart vscode after this." -ForegroundColor Green
