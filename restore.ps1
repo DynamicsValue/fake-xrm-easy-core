bb5c2b4e80024a9ee28a2b7d028c0361b6d62b68
jordimontana82 <jordi.montana@gmail.com>
2021-10-11 09:06:47 +0200

    Added CLA workflow, updated license, added PR template, removed Release configuration

--- restore.ps1
+++ restore.ps1
@@ -17,11 +17,20 @@ Remove-Item -Recurse -Force $localPackageRestoreFolder
 
 Write-Host "Restoring packages..."
 
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY" -targetFrameworks $targetFrameworks
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -targetFrameworks $targetFrameworks
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -targetFrameworks $targetFrameworks
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -targetFrameworks $targetFrameworks
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
-./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
+if($targetFrameworks -eq "netcoreapp3.1")
+{
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
+}
+else 
+{
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -targetFrameworks $targetFrameworks
+    ./restore-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -targetFrameworks $targetFrameworks
+}
+
 
 Write-Host "Done. It's convenient to restart vscode after this." -ForegroundColor Green
