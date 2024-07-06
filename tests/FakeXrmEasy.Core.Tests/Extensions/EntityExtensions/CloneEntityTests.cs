a1f94029c8af92a1f566a707f8c81a1757eb543e
Jordi <jordi.montana@gmail.com>
2024-07-06 14:17:04 +0200

    Adds support for RelatedEntities to Update message, implements cloning of RelatedEntities, extracts and refactors cloning of common properties and adds necessary tests DynamicsValue/fake-xrm-easy#154

---
+++ tests/FakeXrmEasy.Core.Tests/Extensions/EntityExtensions/CloneEntityTests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using Crm;
+using FakeXrmEasy.Extensions;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace FakeXrmEasy.Core.Tests.Extensions
+{
+    public class CloneEntityTests: FakeXrmEasyTestsBase
+    {
+        private readonly Entity _earlyBoundSource;
+        private readonly Entity _lateBoundSource;
+        private Entity _clone;
+        
+        public CloneEntityTests()
+        {
+            _earlyBoundSource = new Account() { Id = Guid.NewGuid() };
+            _lateBoundSource = new Entity("account") { Id = Guid.NewGuid() };
+        }
+
+        [Fact]
+        public void Should_clone_main_late_bound_object_properties()
+        {
+            var clone = _lateBoundSource.Clone(_context);
+            
+            ///Different object reference
+            Assert.NotEqual(clone, _lateBoundSource);
+            
+            Assert.Equal(_lateBoundSource.LogicalName, clone.LogicalName);
+            Assert.Equal(_lateBoundSource.Id, clone.Id);
+        }
+        
+        [Fact]
+        public void Should_clone_main_early_bound_object_properties_and_keep_original_type()
+        {
+            var clone = _earlyBoundSource.Clone(_earlyBoundSource.GetType(), _context);
+            
+            ///Different object reference
+            Assert.NotEqual(clone, _earlyBoundSource);
+            
+            Assert.Equal(_earlyBoundSource.LogicalName, clone.LogicalName);
+            Assert.Equal(_earlyBoundSource.Id, clone.Id);
+
+            Assert.Equal(_earlyBoundSource.GetType(), clone.GetType());
+        }
+
+        [Fact]
+        public void Should_clone_related_entities()
+        {
+            var relatedEntity1 = new Contact() { Id = Guid.NewGuid() };
+            var relatedEntity2 = new Contact() { Id = Guid.NewGuid() };
+            
+            var relationShip = new Relationship("account_contacts");
+            
+            _earlyBoundSource.RelatedEntities = new RelatedEntityCollection()
+            {
+                new KeyValuePair<Relationship, EntityCollection>(
+                    relationShip, 
+                    new EntityCollection(new List<Entity>()
+                {
+                    relatedEntity1, relatedEntity2
+                }))
+            };
+            
+            var clone = _earlyBoundSource.Clone(_earlyBoundSource.GetType(), _context);
+            Assert.NotEqual(clone, _earlyBoundSource);
+
+            Assert.Single(clone.RelatedEntities);
+            
+            var clonedRelatedEntities = clone.RelatedEntities;
+            Assert.True(clonedRelatedEntities.ContainsKey(relationShip));
+
+            var clonedEntityCollection = clonedRelatedEntities[relationShip];
+            Assert.Equal(2, clonedEntityCollection.Entities.Count);
+
+            var clonedRelatedEntity1 = clonedEntityCollection.Entities[0];
+            var clonedRelatedEntity2 = clonedEntityCollection.Entities[1];
+            
+            Assert.NotEqual(relatedEntity1, clonedRelatedEntity1);
+            Assert.Equal(relatedEntity1.Id, clonedRelatedEntity1.Id);
+            Assert.Equal(relatedEntity1.LogicalName, clonedRelatedEntity1.LogicalName);
+            
+            Assert.NotEqual(relatedEntity2, clonedRelatedEntity2);
+            Assert.Equal(relatedEntity2.Id, clonedRelatedEntity2.Id);
+            Assert.Equal(relatedEntity2.LogicalName, clonedRelatedEntity2.LogicalName);
+        }
+    }
+}
--- tests/FakeXrmEasy.Core.Tests/Middleware/Crud/FakeMessageExecutors/CreateRequestTests/CreateRequestTests.cs
+++ tests/FakeXrmEasy.Core.Tests/Middleware/Crud/FakeMessageExecutors/CreateRequestTests/CreateRequestTests.cs
@@ -187,75 +187,7 @@ namespace FakeXrmEasy.Core.Tests.Middleware.Crud.FakeMessageExecutors.CreateRequ
             Assert.Equal(c.Id, contact["contactid"]);
         }
 
-        [Fact]
-        public void When_related_entities_are_used_without_relationship_info_exception_is_raised()
-        {
-            var order = new SalesOrder();
-
-            var orderItems = new EntityCollection(new List<Entity>()
-            {
-                new SalesOrderDetail(),
-                new SalesOrderDetail()
-            });
-
-            // Add related order items so it can be created in one request
-            order.RelatedEntities.Add(new Relationship
-            {
-                PrimaryEntityRole = EntityRole.Referenced,
-                SchemaName = "order_details"
-            }, orderItems);
-
-            var request = new CreateRequest
-            {
-                Target = order
-            };
-
-            var exception = Record.Exception(() => _service.Execute(request));
-
-            Assert.IsType<Exception>(exception);
-            Assert.Equal("Relationship order_details does not exist in the metadata cache", exception.Message);
-        }
-
-        [Fact]
-        public void When_related_entities_and_relationship_are_used_child_entities_are_created()
-        {
-            _context.AddRelationship("order_details",
-                new XrmFakedRelationship()
-                {
-                    Entity1LogicalName = SalesOrder.EntityLogicalName,  //Referenced
-                    Entity1Attribute = "salesorderid",              //Pk
-                    Entity2LogicalName = SalesOrderDetail.EntityLogicalName,
-                    Entity2Attribute = "salesorderid",              //Lookup attribute
-                    RelationshipType = XrmFakedRelationship.FakeRelationshipType.OneToMany
-                });
-
-            var order = new SalesOrder();
-
-            var orderItems = new EntityCollection(new List<Entity>()
-            {
-                new SalesOrderDetail(),
-                new SalesOrderDetail()
-            });
-
-            // Add related order items so it can be created in one request
-            order.RelatedEntities.Add(new Relationship
-            {
-                PrimaryEntityRole = EntityRole.Referenced,
-                SchemaName = "order_details"
-            }, orderItems);
-
-            var request = new CreateRequest
-            {
-                Target = order
-            };
-
-            var id = (_service.Execute(request) as CreateResponse).id;
-            var createdOrderDetails = _context.CreateQuery<SalesOrderDetail>().ToList();
-
-            Assert.Equal(2, createdOrderDetails.Count);
-            Assert.Equal(createdOrderDetails[0].SalesOrderId.Id, id);
-            Assert.Equal(createdOrderDetails[1].SalesOrderId.Id, id);
-        }
+        
 
         [Fact]
         public void Shouldnt_store_references_to_variables_but_actual_clones()
