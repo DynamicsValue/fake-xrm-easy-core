e555c7457b2ee8df814736d820f437e155ee04f8
Jordi <jordi.montana@gmail.com>
2024-08-07 20:51:46 +0200

    Adding more tests for DynamicsValue/fake-xrm-easy#158

--- tests/FakeXrmEasy.Core.Tests/Issues/Issue00158.cs
+++ tests/FakeXrmEasy.Core.Tests/Issues/Issue00158.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Crm;
+using DataverseEntities;
 using FakeXrmEasy.Core.Exceptions.Query.FetchXml;
 using FakeXrmEasy.Extensions;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
 using Xunit;
+using Contact = Crm.Contact;
 
 namespace FakeXrmEasy.Core.Tests.Issues
 {
@@ -33,7 +34,24 @@ namespace FakeXrmEasy.Core.Tests.Issues
             _contactMetadata = new EntityMetadata() { LogicalName = "contact" };
             var fullNameAttribute = new StringAttributeMetadata() { LogicalName = "fullname" };
             var longitudeAttribute = new DoubleAttributeMetadata() { LogicalName = "address1_longitude" };
-            _contactMetadata.SetAttributeCollection(new List<AttributeMetadata>() { fullNameAttribute, longitudeAttribute });
+            var educationCodeAttribute = new PicklistAttributeMetadata()
+            {
+                LogicalName = "educationcode",
+                OptionSet = new OptionSetMetadata(new OptionMetadataCollection(new List<OptionMetadata>()
+                {
+                    new OptionMetadata(new Label("DefaultValue", 1033), 1)
+                }))
+            };
+            var parentContactAttribute = new LookupAttributeMetadata()
+                { LogicalName = "parentcontactid", Targets = new[] { "contact" } };
+            
+            _contactMetadata.SetAttributeCollection(new List<AttributeMetadata>()
+            {
+                fullNameAttribute, 
+                longitudeAttribute,
+                educationCodeAttribute,
+                parentContactAttribute
+            });
         }
 
         [Fact]
@@ -52,7 +70,7 @@ namespace FakeXrmEasy.Core.Tests.Issues
         }
         
         [Fact]
-        public void Should_return_contact_when_using_injected_metadata()
+        public void Should_return_contact_when_using_injected_double_metadata()
         {
             _lateBoundContact["address1_longitude"] = 1.2345;
             _context.Initialize(_lateBoundContact);
@@ -63,6 +81,54 @@ namespace FakeXrmEasy.Core.Tests.Issues
                                     <attribute name='fullname' />
                                         <filter type='and'>
                                             <condition attribute='address1_longitude' operator='eq' value='1.2345' />
+                                        </filter>
+                                  </entity>
+                            </fetch>";
+
+            var entities = _service.RetrieveMultiple(new FetchExpression(fetchXml)).Entities;
+            Assert.Single(entities);
+
+            var contactFound = entities.FirstOrDefault();
+            Assert.Equal(_lateBoundContact.Id, contactFound.Id);
+        }
+        
+        [Fact]
+        public void Should_return_contact_when_using_injected_option_set_metadata()
+        {
+            _lateBoundContact["educationcode"] = new OptionSetValue((int)contact_educationcode.DefaultValue);
+            _context.Initialize(_lateBoundContact);
+            _context.InitializeMetadata(_contactMetadata);
+            
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                              <entity name='contact'>
+                                    <attribute name='fullname' />
+                                        <filter type='and'>
+                                            <condition attribute='educationcode' operator='eq' value='1' />
+                                        </filter>
+                                  </entity>
+                            </fetch>";
+
+            var entities = _service.RetrieveMultiple(new FetchExpression(fetchXml)).Entities;
+            Assert.Single(entities);
+
+            var contactFound = entities.FirstOrDefault();
+            Assert.Equal(_lateBoundContact.Id, contactFound.Id);
+        }
+        
+        [Fact]
+        public void Should_return_contact_when_using_injected_lookup_metadata()
+        {
+            var otherContact = new Entity("contact", Guid.NewGuid());
+            _lateBoundContact["parentcontactid"] = otherContact.ToEntityReference();
+            
+            _context.Initialize(_lateBoundContact);
+            _context.InitializeMetadata(_contactMetadata);
+            
+            var fetchXml = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                              <entity name='contact'>
+                                    <attribute name='fullname' />
+                                        <filter type='and'>
+                                            <condition attribute='parentcontactid' operator='eq' value='{otherContact.Id}' />
                                         </filter>
                                   </entity>
                             </fetch>";
