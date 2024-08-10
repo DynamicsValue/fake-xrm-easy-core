3a74af835b49fff5b3973d5d628d6789d8791a56
Jordi <jordi.montana@gmail.com>
2024-08-10 15:04:36 +0200

    Fix last code smell DynamicsValue/fake-xrm-easy#157

--- src/FakeXrmEasy.Core/FileStorage/Db/Exceptions/FileTokenContinuationNotFoundException.cs
+++ src/FakeXrmEasy.Core/FileStorage/Db/Exceptions/FileTokenContinuationNotFoundException.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace FakeXrmEasy.Core.FileStorage.Db.Exceptions
 {
+    /// <summary>
+    /// Exception thrown when a file continuation token could not be found or invalid, either when initiating a file upload or a file download
+    /// </summary>
     public class FileTokenContinuationNotFoundException: Exception
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="fileContinuationTokenId">The file continuation token that could not be found</param>
         internal FileTokenContinuationNotFoundException(string fileContinuationTokenId) 
             : base($"The file continuation token with Id '{fileContinuationTokenId}' could not be found or has already been committed")
         {
