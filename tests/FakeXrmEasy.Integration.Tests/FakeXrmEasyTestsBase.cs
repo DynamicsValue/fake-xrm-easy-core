747380ae07682a837a1ed4de8056872cbc35d8e2
Jordi <jordi.montana@gmail.com>
2024-09-12 10:26:26 +0200

    Adding missing configurations and update build scripts to due with duplicate Core dependency

---
+++ tests/FakeXrmEasy.Integration.Tests/FakeXrmEasyTestsBase.cs
@@ -0,0 +1,19 @@
+using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Abstractions.Enums;
+using FakeXrmEasy.Middleware;
+using Microsoft.Xrm.Sdk;
+
+namespace FakeXrmEasy.Core.Tests
+{
+    public class FakeXrmEasyTestsBase
+    {
+        protected readonly IXrmFakedContext _context;
+        protected readonly IOrganizationService _service;
+        
+        protected FakeXrmEasyTestsBase()
+        {
+            _context = XrmFakedContextFactory.New(FakeXrmEasyLicense.RPL_1_5);
+            _service = _context.GetOrganizationService();
+        }
+    }
+}
