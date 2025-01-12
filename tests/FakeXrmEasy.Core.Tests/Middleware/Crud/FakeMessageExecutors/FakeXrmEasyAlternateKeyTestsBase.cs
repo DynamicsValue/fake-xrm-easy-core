0e9eb3abad9d891eeb2b14910f684cc2c85bf39c
Jordi <jordi.montana@gmail.com>
2025-01-12 02:26:12 +0100

    Adds precompilation symbols for localized labels

--- tests/FakeXrmEasy.Core.Tests/Middleware/Crud/FakeMessageExecutors/FakeXrmEasyAlternateKeyTestsBase.cs
+++ tests/FakeXrmEasy.Core.Tests/Middleware/Crud/FakeMessageExecutors/FakeXrmEasyAlternateKeyTestsBase.cs
@@ -16,15 +16,23 @@ namespace FakeXrmEasy.Core.Tests.Middleware.Crud.FakeMessageExecutors
             _context.InitializeMetadata(assembly);
             
             var metadata = _context.GetEntityMetadataByName("dv_test");
-            List<LocalizedLabel> otherLocalizedLabels = null;
+            List<LocalizedLabel> otherLocalizedLabels = new List<LocalizedLabel>();
 
             metadata.SetFieldValue("_keys", new EntityKeyMetadata[]
             {
+                #if FAKE_XRM_EASY_9
                 new EntityKeyMetadata()
                 {
                     DisplayName = new Label(new LocalizedLabel("Code", 1033), otherLocalizedLabels),
                     KeyAttributes = new string[]{"dv_code"}
                 }
+                #else
+                new EntityKeyMetadata()
+                {
+                    DisplayName = new Label(new LocalizedLabel("Code", 1033), otherLocalizedLabels.ToArray()),
+                    KeyAttributes = new string[]{"dv_code"}
+                }
+                #endif
             });
             _context.SetEntityMetadata(metadata);
         }
