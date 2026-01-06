b2c7a61333d953316d408c763ee19c1bc3db6f1a
Jordi <jordi.montana@gmail.com>
2026-01-06 11:44:39 +0100

    Adds basic support for CountColumn in QueryExpressions and handling of null in Count and Avg  DynamicsValue/fake-xrm-easy#155 DynamicsValue/fake-xrm-easy#192

---
+++ tests/FakeXrmEasy.Core.Tests/Query/TranslateQueryExpressionTests/AggregationsTests/CountColumnTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using Crm;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Xunit;
+
+namespace FakeXrmEasy.Core.Tests.Query.TranslateQueryExpressionTests.AggregationsTests
+{
+    public class CountTests: FakeXrmEasyTestsBase
+    {
+        
+        [Fact]
+        public void Should_return_correct_count_value_as_an_aliased_value()
+        {
+            // Arrange
+            var numberOfAccounts = 10;
+            var accounts = Enumerable.Range(1, numberOfAccounts)
+                .Select(i => new Account() {
+                    Id = Guid.NewGuid(),
+                    Name = $"Test {i}",
+                    NumberOfEmployees = i
+                })
+                .ToList();
+            
+            //Adds null value
+            accounts.Add(new Account()
+            {
+                Id = Guid.NewGuid(),
+                Name = $"Test 11",
+                NumberOfEmployees = null
+            });
+            
+            _context.Initialize(accounts);
+            
+            QueryExpression query = new QueryExpression("account")
+            {
+                ColumnSet = new ColumnSet(false)
+                {
+                    AttributeExpressions = {
+                        new XrmAttributeExpression{
+                            AttributeName = "numberofemployees",
+                            Alias = "accountcount",
+                            AggregateType = XrmAggregateType.Count
+                        }
+                    }
+                }
+            };
+            
+            var entityCollection = _service.RetrieveMultiple(query);
+            Assert.Single(entityCollection.Entities);
+
+            var resultingEntity = entityCollection.Entities[0];
+            var aggregatedField = resultingEntity["accountcount"];
+            Assert.IsType<AliasedValue>(aggregatedField);
+
+            Assert.Equal(numberOfAccounts + 1, ((AliasedValue) aggregatedField).Value);
+        }
+    }
+}
