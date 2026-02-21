55bc85a129bd520602a174bac5f9e4ea75de0c41
Jordi <jordi.montana@gmail.com>
2026-02-21 15:20:11 +0100

    Adds unit tests for FiscalYear retrieval based on FiscalYearSettings configuration DynamicsValue/fake-xrm-easy#192

--- tests/FakeXrmEasy.Core.Tests/Query/Aggregations/FiscalYearTests.cs
+++ tests/FakeXrmEasy.Core.Tests/Query/Aggregations/FiscalYearTests.cs
@@ -1,4 +1,5 @@
 using System;
+using FakeXrmEasy.Abstractions.Settings;
 using FakeXrmEasy.Core.Query.Aggregations;
 using Xunit;
 
@@ -6,11 +7,34 @@ namespace FakeXrmEasy.Core.Tests.Query.Aggregations
 {
     public class FiscalYearTests: FakeXrmEasyTestsBase
     {
-        [Fact]
-        public void Should_return_2026_for_Jan_1st()
+        [Theory]
+        [InlineData(2026, 2026,1,1)]
+        [InlineData(2026, 2026,4,1)]
+        [InlineData(2026, 2026,2,28)]
+        [InlineData(2026, 2026,12,31)]
+        [InlineData(2024, 2024,2,29)]
+        public void Should_return_expected_fiscal_year_with_default_fiscal_year_settings(int expectedYear, int year, int month, int day)
         {
-            var date = new DateTime(2026, 1, 1);
-            Assert.Equal(2026, FiscalYear.GetFiscalYear(_context, date));
+            var date = new DateTime(year, month, day);
+            Assert.Equal(expectedYear, FiscalYear.GetFiscalYear(_context, date));
+        }
+        
+        [Theory]
+        [InlineData(2025, 2026,1,1)]
+        [InlineData(2025, 2026,4,1)]
+        [InlineData(2025, 2026,6,30)] //last day of fiscal year
+        [InlineData(2026, 2026,7,1)] //first day of fiscal year
+        [InlineData(2026, 2026,12,31)]
+        [InlineData(2026, 2027,6,30)] //last day of fiscal year
+        [InlineData(2023, 2024,2,29)]
+        public void Should_return_expected_fiscal_year_non_default_fiscal_year_settings(int expectedYear, int year, int month, int day)
+        {
+            _context.SetProperty<FiscalYearSettings>(new FiscalYearSettings()
+            {
+                StartDate = new DateTime(2026, 7, 1) //July 1st
+            });
+            var date = new DateTime(year, month, day);
+            Assert.Equal(expectedYear, FiscalYear.GetFiscalYear(_context, date));
         }
     }
 }
