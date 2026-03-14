837955f4cd7b57a3e51b6901374b14ea7514ff27
Jordi <jordi.montana@gmail.com>
2026-03-14 13:16:06 +0100

    Add a few more test cases

--- tests/FakeXrmEasy.Core.Tests/Extensions/ObjectExtensionsTests.cs
+++ tests/FakeXrmEasy.Core.Tests/Extensions/ObjectExtensionsTests.cs
@@ -13,5 +13,31 @@ namespace FakeXrmEasy.Core.Tests.Extensions
             var metadata = new EntityMetadata();
             Assert.Throws<ArgumentOutOfRangeException>(() => metadata.SetFieldValue("non-existing", 1));
         }
+        
+        [Fact]
+        public void Should_throw_exception_when_getting_an_unknown_field()
+        {
+            var metadata = new EntityMetadata();
+            Assert.Throws<ArgumentOutOfRangeException>(() => metadata.GetFieldValue("non-existing"));
+        }
+     
+        [Fact]
+        public void Should_set_entity_metadata_field_value()
+        {
+            var metadata = new EntityMetadata();
+            metadata.SetFieldValue("_objectTypeCode", 1);
+            
+            Assert.Equal(1, metadata.ObjectTypeCode);
+        }
+        
+        [Fact]
+        public void Should_get_entity_metadata_field_value()
+        {
+            var metadata = new EntityMetadata();
+            metadata.SetFieldValue("_objectTypeCode", 1);
+            
+            var objectTypeCode = metadata.GetFieldValue("_objectTypeCode");
+            Assert.Equal(1, objectTypeCode);
+        }
     }
 }
