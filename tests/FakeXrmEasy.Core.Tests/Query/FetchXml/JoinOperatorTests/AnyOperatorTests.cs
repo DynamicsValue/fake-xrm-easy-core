8cc555a80ca3f453ac62ba86203915c4c2e0e4a7
Jordi <jordi.montana@gmail.com>
2026-07-17 13:22:11 +0200

    Should resolve DynamicsValue/fake-xrm-easy#260

--- tests/FakeXrmEasy.Core.Tests/Query/FetchXml/JoinOperatorTests/AnyOperatorTests.cs
+++ tests/FakeXrmEasy.Core.Tests/Query/FetchXml/JoinOperatorTests/AnyOperatorTests.cs
@@ -162,6 +162,60 @@ namespace FakeXrmEasy.Core.Tests.Query.FetchXml.JoinOperatorTests
             var result = _service.RetrieveMultiple(new FetchExpression(fetch));
             Assert.Empty(result.Entities);
         }
+        
+        [Fact]
+        public void Should_not_return_contact_records_with_any_operator_if_link_criteria_with_the_and_operator_does_not_match_nested_directly_under_entity()
+        {
+            _context.Initialize(new List<Entity>() {_contact, _contosoAccount });
+            
+            //It's impossible the same account record can have 2 different names, it's used to return no matching records
+            
+            var fetch = @"
+                    <fetch distinct='false' useraworderby='false' no-lock='false' mapping='logical'>
+                        <entity name='contact'>
+                            <attribute name='firstname' />
+                            <link-entity name='account' to='contactid' from='primarycontactid' link-type='any'>
+                                <filter type='and'>
+                                    <condition attribute='name' operator='eq' value='Cont' />
+                                    <condition attribute='name' operator='eq' value='Some other' />
+                                </filter>
+                            </link-entity>
+                            <filter type='or'>
+                                <condition attribute='statecode' operator='eq' value='1' />
+                            </filter>
+                        </entity>
+                   </fetch>
+                ";
+            
+            var result = _service.RetrieveMultiple(new FetchExpression(fetch));
+            Assert.Empty(result.Entities);
+        }
+        
+        [Fact]
+        public void Should_return_contact_with_any_operator_that_matches_an_account_record_nested_directly_under_entity()
+        {
+            _context.Initialize(new List<Entity>() {_contact, _contosoAccount });
+            
+            var fetch = @"
+                    <fetch distinct='false' useraworderby='false' no-lock='false' mapping='logical'>
+                        <entity name='contact'>
+                            <attribute name='firstname' />
+                            <link-entity name='account' to='contactid' from='primarycontactid' link-type='any'>
+                                <filter type='and'>
+                                    <condition attribute='name' operator='eq' value='Contoso' />
+                                </filter>
+                            </link-entity>
+                        </entity>
+                   </fetch>
+                ";
+
+            var result = _service.RetrieveMultiple(new FetchExpression(fetch));
+            Assert.Single(result.Entities);
+            Assert.Equal("Joe", result.Entities[0]["firstname"]);
+        }
+        
+        
+        
     }
 }
 #endif
